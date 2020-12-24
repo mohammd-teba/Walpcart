7f462040cb30f3f4be468d190f6559389a99a34d
Khaled-AlReqeb <khaleds1work@gmail.com>
2020-12-24 20:32:24 +0200

    finalup

---
+++ resources/views/website/home/index.blade.php
@@ -0,0 +1,220 @@
+@extends('website.layout.index')
+
+
+@section('content')
+    <div class="section text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 ml-auto mr-auto">
+                    <h2 class="title" style="color: #3ec0b9;font-family: walpcartSTC;font-size: 40px;">Service</h2>
+                    <h5 class="description" style="font-family: walpcartSTC"> Last service was added </h5>
+                </div>
+            </div>
+            <br>
+            <br>
+            <div class="text-left">
+                <div class="row">
+                        <div class="col-lg-4">
+                            <div class="card" style="width: 20rem;">
+                                <a href="" class="col-md-6  mr-auto ml-auto">
+                                    <img src="{{url(assets('website'))}}/img/faces/kaci-baum-2.jpg" class="img-circle img-no-padding img-responsive" alt="Rounded Image">
+                                    <p class="text-center" style="font-family: walpcartSTC">Brigitte Bardot</p>
+
+                                </a>
+
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a href="" title="" style="font-family: walpcartSTC">
+                                            This words its just for test  test test test This words just for test  test test test This words just for test  test test test
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div><div class="col-lg-4">
+                            <div class="card" style="width: 20rem;">
+                                <a href="" class="col-md-6  mr-auto ml-auto">
+                                    <img src="{{url(assets('website'))}}/img/faces/kaci-baum-2.jpg" class="img-circle img-no-padding img-responsive" alt="Rounded Image">
+                                    <p class="text-center" style="font-family: walpcartSTC">Brigitte Bardot</p>
+
+                                </a>
+{{--                                <a href="">--}}
+{{--                                    <img class="card-img-top" src="{{url(assets('website'))}}/img/soroush-karimi.jpg" alt="" style="max-height: 160px">--}}
+{{--                                </a>--}}
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a href="" title="" style="font-family: walpcartSTC">
+                                            This words just for test  test test test This words just for test  test test test This words just for test  test test test
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div><div class="col-lg-4">
+                            <div class="card" style="width: 20rem;">
+                                <div class="col-md-6  mr-auto ml-auto">
+                                    <img src="{{url(assets('website'))}}/img/faces/kaci-baum-2.jpg" class="img-circle img-no-padding img-responsive" alt="Rounded Image">
+                                    <p class="text-center" style="font-family: walpcartSTC">Brigitte Bardot</p>
+
+                                </div>
+{{--                                <a href="">--}}
+{{--                                    <img class="card-img-top" src="{{url(assets('website'))}}/img/soroush-karimi.jpg" alt="" style="max-height: 160px">--}}
+{{--                                </a>--}}
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a href="" title="" style="font-family: walpcartSTC">
+                                            This words just for test  test test test This words just for test  test test test This words just for test  test test test
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div><div class="col-lg-4">
+                            <div class="card" style="width: 20rem;">
+                                <a href="" class="col-md-6  mr-auto ml-auto">
+                                    <img src="{{url(assets('website'))}}/img/faces/kaci-baum-2.jpg" class="img-circle img-no-padding img-responsive" alt="Rounded Image">
+                                    <p class="text-center" style="font-family: walpcartSTC">Brigitte Bardot</p>
+
+                                </a>
+{{--                                <a href="">--}}
+{{--                                    <img class="card-img-top" src="{{url(assets('website'))}}/img/soroush-karimi.jpg" alt="" style="max-height: 160px">--}}
+{{--                                </a>--}}
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a href="" title="" style="font-family: walpcartSTC">
+                                            This words just for test  test test test This words just for test  test test test This words just for test  test test test
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div><div class="col-lg-4">
+                            <div class="card" style="width: 20rem;">
+                                <a href="" class="col-md-6  mr-auto ml-auto">
+                                    <img src="{{url(assets('website'))}}/img/faces/kaci-baum-2.jpg" class="img-circle img-no-padding img-responsive" alt="Rounded Image">
+                                    <p class="text-center" style="font-family: walpcartSTC">Brigitte Bardot</p>
+
+                                </a>
+{{--                                <a href="">--}}
+{{--                                    <img class="card-img-top" src="{{url(assets('website'))}}/img/soroush-karimi.jpg" alt="" style="max-height: 160px">--}}
+{{--                                </a>--}}
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a href="" title="" style="font-family: walpcartSTC">
+                                            This words just for test  test test test This words just for test  test test test This words just for test  test test test
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    <div class="col-lg-4">
+                            <div class="card" style="width: 20rem;">
+
+                                <a href="" class="col-md-6  mr-auto ml-auto">
+                                    <img src="{{url(assets('website'))}}/img/faces/kaci-baum-2.jpg" class="img-circle img-no-padding img-responsive" alt="Rounded Image">
+                                    <p class="text-center" style="font-family: walpcartSTC">Brigitte Bardot</p>
+
+                                </a>
+
+                                <div class="card-body">
+                                    <p class="card-text">
+                                        <a href="" title="" style="font-family: walpcartSTC">
+                                            This words just for test  test test test This words just for test  test test test This words just for test  test test test
+                                        </a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+
+                </div>
+
+            </div>
+            <br>
+
+            <div class="section">
+                <hr>
+                <div class="container">
+                    <h3 class="title" style="text-align: center;color: #3ec0b9;font-family: walpcartSTC;font-size: 40px;">About Us</h3>
+                    <div class="row">
+                     <p style="color: black;font-family: walpcartSTC">Walpcart is a platform that collects local and international brands in one place. We provide online store to the local market to improve customer service. We are making sure that every customer is satisfied </p>
+                    </div>
+                </div>
+            </div>
+            <hr>
+        </div>
+    </div>
+
+    <div class="section landing-section">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-8 ml-auto mr-auto">
+                    <h2 class="text-center" style="color: #3ec0b9;font-family: walpcartSTC;font-size: 40px;">Keep in touch?</h2>
+                    <form class="contact-form" action="">
+
+{{--                    <form class="contact-form" action="{{ route('contact.store') }}">--}}
+                        <div class="row">
+                            <div class="col-md-12">
+                                <label style="font-family: walpcartSTC">Name</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                     <span class="input-group-text">
+                                           <i class="nc-icon nc-single-02"></i>
+                                      </span>
+                                    </div>
+                                    <input type="text" name="name"  required class="form-control @error('name') is-invalid @enderror" placeholder="Name">
+
+                                </div>
+                            </div>
+
+{{--                                <div class="col-md-3" style="margin-top: 40px;">--}}
+{{--                                    <p style="color: black;font-family: walpcartSTC"> <i class="nc-icon nc-pin-3"></i>  Muscat, Oman</p>--}}
+{{--                                    <br>--}}
+{{--                                    <a href="" style="color: #0b94ea;font-size: 14px;font-family: walpcartSTC"><i class="nc-icon nc-email-85"></i> info@walpcart.com</a>--}}
+{{--                                </div>--}}
+
+
+
+                            <div class="col-md-12">
+                                <label style="font-family: walpcartSTC">Email</label>
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                     <span class="input-group-text">
+                                           <i class="nc-icon nc-email-85"></i>
+                                     </span>
+                                    </div>
+                                    @php $input = "email"; @endphp
+                                    <input type="email" name="{{ $input }}" required class="form-control @error($input) is-invalid @enderror"  placeholder="Email">
+                                    @error($input)
+                                    <span class="invalid-feedback" role="alert">
+                                         <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <label style="font-family: walpcartSTC">Message</label>
+
+                        @php $input = "message"; @endphp
+                        <textarea class="form-control @error($input) is-invalid @enderror" name="{{ $input }}" required rows="4" placeholder="Tell us your thoughts and feelings..."></textarea>
+                        @error($input)
+                        <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                        </span>
+                        @enderror
+
+{{--                        <div class="row">--}}
+{{--                            <div class="col-md-4 ml-auto mr-auto">--}}
+{{--                                <button class="btn btn-sm btn-fill" style="font-family: Arial ;color: #ffffff;background-color: #0b1011;border-style: none">Send message</button>--}}
+{{--                            </div>--}}
+{{--                        </div>--}}
+                    </form>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
+
+@endsection

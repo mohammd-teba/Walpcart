4e4b63a7ddd65908da2d03bfc0ddccf17f7a246c
Khaled-AlReqeb <khaleds1work@gmail.com>
2020-12-25 00:50:35 +0200

    khaledfooter

--- resources/views/website/layout/footer.blade.php
+++ resources/views/website/layout/footer.blade.php
@@ -1,27 +1,30 @@
 <footer class="footer footer-black  footer-white ">
-    <div class="container">
+    <div class="container-fluid" style="background-color: #0b1011;height: 100%">
         <div class="row">
-            <nav class="footer-nav">
-                <ul>
-                    <li>
-                        <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
-                    </li>
-                    <li>
-                        <a href="http://blog.creative-tim.com/" target="_blank">Blog</a>
-                    </li>
-                    <li>
-                        <a href="https://www.creative-tim.com/license" target="_blank">Licenses</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="credits ml-auto">
-            <span class="copyright">
-              ©
-              <script>
-                document.write(new Date().getFullYear())
-              </script>, made with <i class="fa fa-heart heart"></i> by Creative Tim
-            </span>
+           <div class="col-md-4" style="margin-top: 30px">
+               <h6 style="font-family: walpcartSTC;color: #3ec0b9;font-size: 20px">Service</h6>
+               <p style="font-family: walpcartSTC;color: #ffffff;margin-left: 15px">Any service name</p>
+               <p style="font-family: walpcartSTC;color: #ffffff;margin-left: 15px">Any service name</p>
+               <p style="font-family: walpcartSTC;color: #ffffff;margin-left: 15px">Any service name</p>
+           </div>
+            <div class="col-md-4" style="margin-top: 30px">
+              <h6 style="font-family: walpcartSTC;color: #3ec0b9;font-size: 20px">Product</h6>
+                <p style="font-family: walpcartSTC;color: #ffffff;margin-left: 25px">Any product name</p>
+                <p style="font-family: walpcartSTC;color: #ffffff;margin-left: 25px">Any product name</p>
+                <p style="font-family: walpcartSTC;color: #ffffff;margin-left: 25px">Any product name</p>
+           </div>
+            <div class="col-md-4"style="margin-top: 30px" >
+              <h6 style="font-family: walpcartSTC;color: #3ec0b9;font-size: 20px">Information</h6>
+                <p style="color: #ffffff;font-family: walpcartSTC"> <i class="nc-icon nc-pin-3"></i>  Muscat, Oman</p>
+                <a href="" style="color: #0b94ea;font-size: 14px;font-family: walpcartSTC"><i class="nc-icon nc-email-85"></i> info@walpcart.com</a>
+           </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <img src="{{url(assets('website'))}}/img/logo2.png" style="width: 10.5%;float: right;margin-top: -120px" alt="">
             </div>
         </div>
+        <br>
     </div>
 </footer>

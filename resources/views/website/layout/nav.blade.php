112ec55a582a6237d2291c0a5642c1f9274c192e
Khaled-AlReqeb <khaleds1work@gmail.com>
2020-12-24 20:44:36 +0200

    teba

--- resources/views/website/layout/nav.blade.php
+++ resources/views/website/layout/nav.blade.php
@@ -1,13 +1,10 @@
 <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="-10">
     <div class="container">
         <div class="navbar-translate">
-<<<<<<< HEAD
-            <a class="navbar-brand" href="https://demos.creative-tim.com/paper-kit/index.html" rel="tooltip" title="Coded by Creative Tim" data-placement="bottom" target="_blank">
-                <img src="{{url(assets('website'))}}/img/logo2.png" style="width: 9.5%;" alt="">            </a>
-=======
+
             <a class="navbar-brand" href="{{ route('home') }}" rel="tooltip" title="Coded by Creative Tim" data-placement="bottom" target="_blank">
                 <img src="{{url(assets('website'))}}/img/logo2.png" style="width: 8.5%;" alt="">            </a>
->>>>>>> 96f440ce4fc1bbb1db9fdd4fba2cc75e965e9dcf
+
 
             <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-bar bar1"></span>
